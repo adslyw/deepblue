0a7d57812d508c935473298523962025b7bdd20f
adslyw <adslyw@gmail.com>
2015-03-03 17:34:08 +0800

    modifiy mission's views

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -4,7 +4,7 @@ class MissionsController < ApplicationController
   respond_to :html
 
   def index
-    @missions = Mission.all
+    @missions = Mission.paginate(:page => params[:page], :per_page => 10).order('created_at DESC')
     respond_with(@missions)
   end
 
