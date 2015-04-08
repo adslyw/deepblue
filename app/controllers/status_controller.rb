3109ea880780c3c63a91cb5cddd7f059dfb00c17
adslyw <adslyw@gmail.com>
2015-04-08 16:27:48 +0800

    change status controller and search controller

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,8 +2,8 @@ class StatusController < ApplicationController
   def index
     begin
       @bcv = BcvService.new
-      dblink_exist = !@bcv.process(Mission.find(1).to_hash).empty?
-      dbupdate_time = @bcv.process(Mission.find(2).to_hash).first.item_value
+      dblink_exist = !@bcv.exec_mission(Mission.find(1)).empty?
+      dbupdate_time = @bcv.exec_mission(Mission.find(2)).first.item_value
       @status = OpenStruct.new
       @status.updated = true
       @status.dblink_exist = dblink_exist
